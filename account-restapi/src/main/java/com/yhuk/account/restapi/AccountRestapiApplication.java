a5f957be410fbb36e6f3e0a3deb579ec4022760b
liugaozhi <Gaozhi.Liu@b2bpas.com>
2019-06-14 19:09:28 +0800

    up

--- account-restapi/src/main/java/com/yhuk/account/restapi/AccountRestapiApplication.java
+++ account-restapi/src/main/java/com/yhuk/account/restapi/AccountRestapiApplication.java
@@ -6,15 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan(basePackages ={"com.yhuk.account"})
 @MapperScan("com.yhuk.account.*.dao")
 @EnableDiscoveryClient(autoRegister = true)  //单机测试不注册改为 false
 public class AccountRestapiApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(AccountRestapiApplication.class, args);
     }
---
+++ account-restapi/src/main/java/com/yhuk/account/restapi/config/SwaggerConfig.java
@@ -0,0 +1,55 @@
+package com.yhuk.account.restapi.config;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description TODO
+ * @Author gaozhi.liu
+ * @Date 2019/6/14 19:03
+ * @Version 1.0
+ **/
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<Parameter>();
+        tokenPar.name("accessToken").description("AccessToken令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.yhuk.account.restapi.controller"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build().globalOperationParameters(pars);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("springboot利用swagger构建api文档")
+                .description("简单优雅的restfun风")
+                .version("2.0")
+                .build();
+    }
+}
